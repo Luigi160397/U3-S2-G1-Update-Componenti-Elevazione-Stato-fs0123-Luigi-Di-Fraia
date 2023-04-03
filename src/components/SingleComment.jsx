a23fb91c3ad8761c7b01272e8eb5cbf940b9b614
Luigi160397 <luigi.difraia.97@gmail.com>
2023-04-03 17:52:43 +0200

    css

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -7,8 +7,8 @@ class SingleComment extends Component {
       <ListGroupItem>
         <Row className="d-flex justify-content-between align-items-center">
           <Col xs={4}>Autore: {this.props.author}</Col>
-          <Col xs={6}>{this.props.comment}</Col>
-          <Col xs={2}>
+          <Col xs={5}>{this.props.comment}</Col>
+          <Col xs={3}>
             Rate: <Badge bg="dark">{this.props.rate}</Badge>
           </Col>
         </Row>
